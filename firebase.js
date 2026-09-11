21f1a8851d64d3270267001943186140d3819882
joyce-codes <joycerautela@gmail.com>
2026-09-11 15:25:59 -0400

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDAjLbcTFQXEGjwU1Hk3PnXhgzyb9U18b0",
+    authDomain: "vaycount.firebaseapp.com",
+    projectId: "vaycount",
+    storageBucket: "vaycount.appspot.com",
+    messagingSenderId: "9577181727",
+    appId: "1:9577181727:web:c5107688f0841b26c6db2a",
+    measurementId: "G-R1Z5W03QQ9"
+  };
